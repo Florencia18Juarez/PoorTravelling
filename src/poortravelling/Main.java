d89dbc41f678d77364c2662a96b06256a1789cfa
silva <silva@LAPTOP-51IP593M>
2019-06-05 14:08:48 -0300

    prueba dos

--- src/poortravelling/Main.java
+++ src/poortravelling/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        System.out.println("poortravelling.Main.main()");
     }
     
 }
